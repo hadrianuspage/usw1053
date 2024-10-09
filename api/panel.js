800fa6b53b9238827ba69306e58b69c913773105
hadrianuspage <wibowos27@gmail.com>
2024-10-09 10:03:48 +0700

    Update and rename panel.js to panel.js

---
+++ api/panel.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    const data = {
+        "_id": "6705c3d99e268b320ab92215",
+        "name": "ShinobiWarfare1053UnlockEverything",
+        "code": "usw1053",
+        "status": true,
+        "__v": 0
+    };
+    res.status(200).json(data);
+}
